bbac18cd8aeb1844f1a5b71b01b46d0d342deaf5
Tú Hình <htinhtu@gmail.com>
2022-12-26 17:05:21 +0700

    change setting

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -1,7 +1,7 @@
 export default {
 	title: `Liva Grup Vietnam | Nhà Phân Phối Chính kim Thu Sét Liva Tại Việt Nam`,
 	description: `Chúng tôi là nhà phân phối chính kim thu sét LIVA tại Việt Nam. Kim thu sét LIVA được sản xuất bởi tập đoàn Liva Grup của Thổ Nhĩ Kỳ...`,
-	url: `https://tuht97.github.io`, // No trailing slash!
+	url: `https://livagrup.com.vn`, // No trailing slash!
 	name: `LivaGrupVN`, // The short name of the business or brand name. Used for things like the copyright in the footer.
 	enableThemeSwitcher: true,
 	showLittleSticksPlug: true, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
